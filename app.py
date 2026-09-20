356e9fe6cbbcf52ead2e7a266601a17676e32741
Dianela <121925422+dianela07@users.noreply.github.com>
2026-09-19 20:23:36 -0400

    Update model name in chat completion request

--- app.py
+++ app.py
@@ -265,7 +265,7 @@ elif st.session_state.paso == 5:
             
             chat_completion = client.chat.completions.create(
                 messages=[{"role": "user", "content": prompt}],
-                model="llama3-8b-8192", 
+                model="llama-3.1-8b-instant", 
             )
             
             resultado = chat_completion.choices[0].message.content
